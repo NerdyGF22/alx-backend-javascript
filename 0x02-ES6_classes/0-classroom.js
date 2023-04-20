ae163d7a0a45ae82156a2cd6e9769ff5d9f5af9d
TelmaFarida <Telmafarida.tg@gmail.com>
2023-04-20 10:37:37 +0000

    creating a class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+A readme file for tasks that will help in better understanding classes in ECMA -2015
