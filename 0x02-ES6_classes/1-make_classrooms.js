8c17700deacaa0b18e86c00c15da8087e8142ac6
TelmaFarida <Telmafarida.tg@gmail.com>
2023-04-20 10:41:11 +0000

    more classes

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+/**
+ * @returns An array of {@link ClassRoom}s.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
