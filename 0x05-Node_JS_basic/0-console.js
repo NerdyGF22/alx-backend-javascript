640d254e11f10e3264afd213e703f1715fa78ede
NerdyGF22 <telmafarida.tg@gmail.com>
2023-06-13 20:12:05 +0000

    0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Telma Farida Github=> NerdyGF22
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+A readme file for backend javascrpt that uses node js to communicate with the api for smooth operation 
