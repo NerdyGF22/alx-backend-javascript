6db4092d07037d10b0631c5c57c4742b126ac745
NerdyGF22 <telmafarida.tg@gmail.com>
2023-06-13 20:31:47 +0000

    fixed bugs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,7 @@
+/**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Telma Farida Github=> NerdyGF22
+ * @author Telma Farida github=>NerdyGF22
  */
 const displayMessage = (msg) => {
   console.log(msg);
